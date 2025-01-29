60617f62522dae68122a18ddc1727129e56f5a19
kotireddy13 <koteshwarreddygogula@gmail.com>
2025-01-29 22:31:51 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,22 @@
+public class datatypes {
+    public static void main(String[] args) {
+       byte number = 100;
+        System.out.println("Byte = "+number);
+        short num = 4590;
+        System.out.println("Short = "+num);
+        long n = 450422590;
+        System.out.println("Long = "+n);
+        char ch = 'A';
+        System.out.println("Character = "+ch);
+        String string = "Akhi";
+        System.out.println("String = "+string);
+        int a = 23;
+        System.out.println("Integer = "+a);
+        float f = 12.456f;
+        System.out.println("Float = "+f);
+        double d = 4567.08f;
+        System.out.println("Doube = "+d);
+        boolean b = 13 < 14;
+        System.out.println("Boolean = "+b);
+    }
+}
